234f9a2541f71cfedf23c1cbed23e98864db1abb
gireeshgg <34162819+gireeshgg@users.noreply.github.com>
2018-12-21 19:40:11 +0530

    Update App.java

--- src/main/java/com/mindtree/assignment2new/App.java
+++ src/main/java/com/mindtree/assignment2new/App.java
@@ -13,7 +13,7 @@ public class App
     {
         System.out.println( "Hello World!" );
         
-        Employee e = new Employee();
+        Employee e = new Employekmkonmkojnmkoknmkolm kojnme();
         e.setId(156);  
 	    e.setFullname("varun");  
 	    e.setDob("dfghj");
